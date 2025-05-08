0a2aa4f14b53603a09432cd93b141b6f82e95807
Tigo-cmd <ja16162040@gmail.com>
2025-05-08 01:33:35 +0100

    sent apikey for infura securly

--- model/generate_wallet.py
+++ model/generate_wallet.py
@@ -1,11 +1,16 @@
 from web3 import Web3
 import secrets
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# Load environment variables
 
+INFURA_KEY = os.getenv('INFURA_KEY')
 
 async def generate_wallet()->(str, str):
   # Generate a new Ethereum wallet
 
-  w3 = Web3(Web3.HTTPProvider("https://swellchain-mainnet.infura.io/v3/5460cf9251af48b1bd909965c50c9adf/"))
+  w3 = Web3(Web3.HTTPProvider(f"https://swellchain-mainnet.infura.io/v3/{INFURA_KEY}/"))
   private_key = "0x" + secrets.token_hex(32)
   account = w3.eth.account.from_key(private_key)
   address = account.address
